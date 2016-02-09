2e049310970f986ac6b798f45b5250facb3602b8
EmulatorCentral <jordan@thesmokinggnu.us>
2016-02-09 15:59:03 -0600

    Added a self-test
    Testing all required functions

---
+++ selftest.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import lcd as LCD
+import time
+LCD.main()
+LCD.lcd_init()
+time.sleep(1)
+LCD.lcd_string("line 1",LCD.LCD_LINE_1)
+LCD.lcd_string("line 2",LCD.LCD_LINE_2)
+time.sleep(2)
+LCD.lcd_string("Animation test",LCD.LCD_LINE_2)
+LCD.lcd_string("               T",LCD.LCD_LINE_1)
+
+LCD.lcd_string("              T ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("             T  ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("            T   ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("           T    ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("          T     ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("         T      ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("        T       ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("       T        ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("      T         ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("     T          ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("    T           ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("   T            ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("  T             ",LCD.LCD_LINE_1)
+
+LCD.lcd_string(" T              ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("T               ",LCD.LCD_LINE_1)
+time.sleep(2)
+LCD.lcd_string("T              e",LCD.LCD_LINE_1)
+
+LCD.lcd_string("T             e ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("T            e  ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("T           e   ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("T          e    ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("T         e     ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("T        e      ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("T       e       ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("T      e        ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("T     e         ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("T    e          ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("T   e           ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("T  e            ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("T e             ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("Te              ",LCD.LCD_LINE_1)
+time.sleep(2)
+LCD.lcd_string("Te             s",LCD.LCD_LINE_1)
+
+LCD.lcd_string("Te            s ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("Te           s  ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("Te          s   ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("Te         s    ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("Te        s     ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("Te       s      ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("Te      s       ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("Te     s        ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("Te    s         ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("Te   s          ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("Te  s           ",LCD.LCD_LINE_1)
+ 
+LCD.lcd_string("Te s            ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("Tes             ",LCD.LCD_LINE_1)
+time.sleep(2)
+LCD.lcd_string("Tes            t",LCD.LCD_LINE_1)
+
+LCD.lcd_string("Tes           t ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("Tes          t  ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("Tes         t   ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("Tes        t    ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("Tes       t     ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("Tes      t      ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("Tes     t       ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("Tes    t        ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("Tes   t         ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("Tes  t          ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("Tes t           ",LCD.LCD_LINE_1)
+
+LCD.lcd_string("Test            ",LCD.LCD_LINE_1)
+time.sleep(2)
+#ASCII Test
+LCD.lcd_string("_--_",LCD.LCD_LINE_1)
+LCD.lcd_string("",LCD.LCD_LINE_2)
+LCD.lcd_string(" _--_",LCD.LCD_LINE_1)
+LCD.lcd_string("  _--_",LCD.LCD_LINE_1)
+LCD.lcd_string("   _--_",LCD.LCD_LINE_1)
+LCD.lcd_string("    _--_",LCD.LCD_LINE_1)
+LCD.lcd_string("     _--_",LCD.LCD_LINE_1)
+LCD.lcd_string("      _--_",LCD.LCD_LINE_1)
+LCD.lcd_string("       _--_",LCD.LCD_LINE_1)
+LCD.lcd_string("        _--_",LCD.LCD_LINE_1)
+LCD.lcd_string("         _--_",LCD.LCD_LINE_1)
+LCD.lcd_string("          _--_",LCD.LCD_LINE_1)
+LCD.lcd_string("           _--_",LCD.LCD_LINE_1)
+LCD.lcd_string("            _--_",LCD.LCD_LINE_1)
+LCD.lcd_string("             _--",LCD.LCD_LINE_1)
+LCD.lcd_string("_               ",LCD.LCD_LINE_2)
+LCD.lcd_string("              _-",LCD.LCD_LINE_1)
+LCD.lcd_string("-_              ",LCD.LCD_LINE_2)
+LCD.lcd_string("               _",LCD.LCD_LINE_1)
+LCD.lcd_string("--_             ",LCD.LCD_LINE_2)
+LCD.lcd_string("                ",LCD.LCD_LINE_1)
+LCD.lcd_string("_--_            ",LCD.LCD_LINE_2)
+LCD.lcd_string(" _--_           ",LCD.LCD_LINE_2)
+LCD.lcd_string("  _--_          ",LCD.LCD_LINE_2)
+LCD.lcd_string("   _--_         ",LCD.LCD_LINE_2)
+LCD.lcd_string("    _--_        ",LCD.LCD_LINE_2)
+LCD.lcd_string("     _--_       ",LCD.LCD_LINE_2)
+LCD.lcd_string("      _--_      ",LCD.LCD_LINE_2)
+LCD.lcd_string("       _--_     ",LCD.LCD_LINE_2)
+LCD.lcd_string("        _--_    ",LCD.LCD_LINE_2)
+LCD.lcd_string("         _--_   ",LCD.LCD_LINE_2)
+LCD.lcd_string("          _--_  ",LCD.LCD_LINE_2)
+LCD.lcd_string("           _--_ ",LCD.LCD_LINE_2)
+LCD.lcd_string("            _--_",LCD.LCD_LINE_2)
